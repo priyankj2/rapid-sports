0e10e4329143d0ee5f90cfd7bb250b38ba18a858
kameshSharma <kameshs@damcogroup.com>
2023-12-03 19:29:52 +0530

    3rd graph UI need to be done

--- src/app/shared/charts/tempo-rhythm/tempo-rhythm.component.ts
+++ src/app/shared/charts/tempo-rhythm/tempo-rhythm.component.ts
@@ -8,11 +8,16 @@ import { Chart, registerables } from 'chart.js';
 })
 export class TempoRhythmComponent {
   dataArray: any = [];
+  lineWidth: number = 2;
   decimalSequences: number[][] = [];
   sumOfSums: number[] = [];
   // Assuming this is your x-axis array
   xValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
   @ViewChild('areaChart', { static: true }) areaChart?: ElementRef<HTMLCanvasElement>;
+  backSwing!: number;
+  downSwing!: number;
+  totalSwing!: number;
+  rhythm!: number;
   ngOnInit() {
     Chart.register(...registerables);
   }
@@ -52,63 +57,27 @@ export class TempoRhythmComponent {
     const indexOfBiggestYValue = yValues.indexOf(biggestYValue);
     const subArray = this.xValues.slice(0, indexOfBiggestYValue + 1);
     const sumOfXValues = subArray.reduce((acc, curr) => acc + curr, 0);
-    console.log('backwing', sumOfXValues);
+    // console.log('backwing', sumOfXValues);
+    this.backSwing = sumOfXValues;
 
     // Get a subarray from xValues[indexOfBiggestYValue] to the end
     const subArrayFromIndex = this.xValues.slice(indexOfBiggestYValue);
     // Calculate the sum of values within the subarray
     const sumFromIndexToEnd = subArrayFromIndex.reduce((acc, curr) => acc + curr, 0);
 
-    console.log('Downswing', sumFromIndexToEnd);
+    //console.log('Downswing', sumFromIndexToEnd);
+    this.downSwing = sumFromIndexToEnd;
 
-    console.log('Total Swing Duration: ' + (sumOfXValues + sumFromIndexToEnd));
-
-    console.log('Rhythm: ' + (sumOfXValues / sumFromIndexToEnd));
-
+    //console.log('Total Swing Duration: ' + (sumOfXValues + sumFromIndexToEnd));
+    this.totalSwing = sumOfXValues + sumFromIndexToEnd;
+    this.rhythm = sumOfXValues / sumFromIndexToEnd;
+    //console.log('Rhythm: ' + (sumOfXValues / sumFromIndexToEnd));
+    this.changeLineWidth(this.backSwing);
     //
   }
-  ngAfterViewInit() {
-    let data: any,
-      options: any,
-      chart: any,
-      ctx: any = document.getElementById('areaChart3') as HTMLElement;
 
-    data = {
-      labels: ['Apples', 'Oranges', 'Mixed Fruit'],
-      datasets: [
-        {
-          label: 'Apples',
-          data: [0, 50, 45, 100],
-          backgroundColor: 'rgba(40,125,200,.5)',
-          borderColor: 'rgb(40,100,200)',
-          fill: true,
-          lineTension: 0,
-          radius: 5,
-        },
-        {
-          label: 'Oranges',
-          data: [30, 90, 111, 20],
-          backgroundColor: 'rgba(75,10,125,.5)',
-          borderColor: 'rgb(75,10,125)',
-          fill: true,
-          lineTension: 0.2,
-          radius: 5,
-        },
-      ],
-    };
-
-    options = {
-      responsive: true,
-      maintainAspectRatio: false,
-      legend: {
-        display: false,
-      },
-    };
-
-    chart = new Chart(ctx, {
-      type: 'line',
-      data: data,
-      options: options,
-    });
+  changeLineWidth(newWidth: number) {
+    this.lineWidth = 2000 / newWidth;;
   }
+
 }
